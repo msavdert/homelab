3dd5ec8a61ca1522327c02b58666bad504fe24d8
msavdert <github@savdert.com>
2026-05-01 01:53:19 +0000

    feat: migrate cilium management to argocd

--- terraform/proxmox/outputs.tf
+++ terraform/proxmox/outputs.tf
@@ -1,9 +1,14 @@
+output "kubeconfig" {
+  value     = talos_cluster_kubeconfig.this.kubeconfig_raw
+  sensitive = true
+}
+
 output "talosconfig" {
   value     = data.talos_client_configuration.this.talos_config
   sensitive = true
 }
 
-output "kubeconfig" {
-  value     = talos_cluster_kubeconfig.this.kubeconfig_raw
+output "cilium_manifest" {
+  value     = data.helm_template.cilium.manifest
   sensitive = true
 }
